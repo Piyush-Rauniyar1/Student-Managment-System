ef25205b3972792d3ed22353b429f91fecdc7b3f
RAUNified <piyushrauniyar16@gmail.com>
2025-12-31 17:03:07 +0545

    merched brance of module-attendance

---
+++ src/attendance/Connection.java
@@ -0,0 +1,12 @@
+package attendance;
+
+import java.sql.PreparedStatement;
+
+public class Connection {
+
+	public PreparedStatement prepareStatement(String sql) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
