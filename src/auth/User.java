bc302eb2f3076b30dd86ec8860be9fadb4060052
ronishkarki-stact <np02cs4a240026@bicnepal.edu.np>
2025-12-31 14:04:34 +0545

    add UI

--- src/auth/User.java
+++ src/auth/User.java
@@ -5,21 +5,36 @@ public class User {
     String userName;
     String password;
 
+
     public User(int userID, String userName, String password) {
         this.userID = userID;
         this.userName = userName;
+        this.password = password;
     }
+    public User(){
+
+    }
+
 
     public int getUserId() {
         return userID;
     }
+    public void setUserId(int userID) {
+        this.userID = userID;
+    }
 
     public String getUsername() {
         return userName;
+    }
+    public void setUsername(String userName) {
+        this.userName = userName;
     }
 
     public String getPassword() {
         return password;
     }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
