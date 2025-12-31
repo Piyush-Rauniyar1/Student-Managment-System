fa86d9071e60e8b318ecb416fd1a834bb58f04c8
RAUNified <piyushrauniyar16@gmail.com>
2025-12-31 13:37:06 +0545

    addes ReportDAO.java

--- src/reports/ReportDAO.java
+++ src/reports/ReportDAO.java
@@ -1,4 +1,15 @@
 package reports;
 
-public class ReportDAO {
+import java.util.List;
+import java.util.Map;
+import course.Course;
+import student.Student;
+
+public interface ReportDAO {
+
+    StudentReport generateStudentReport(int studentId);
+
+    Map<Course, Double> generateCourseAttendanceReport(int courseId);
+
+    List<Student> getDefaulterList(double minAttendance);
 }
